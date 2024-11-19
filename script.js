7df2e092d32134202b04821386410433f76ca593
NiveditaThanvi411 <niveditathanvi@gmail.com>
2024-11-19 23:31:52 +0530

    changes added

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Text Converter</title>
-    <link rel="stylesheet" href="./styles.css" />
-</head>
-<body>
-    <h1 class="text-center">Text Converter</h1>
-    <input type="text" id="input" class="textbox" placeholder="Enter a text to perform a text operation" />
-    <input type="text" id="output" class="textbox" placeholder="Output will be displayed here" />
-    <script src="./script.js"></script>
+const inputElement = document.getElementById('input');
+const outputElement = document.getElementById('output');
 
-    <div class="btn-container">
-        <button type="button" class="action-btns" onclick="handleUpperCase()">UpperCase</button>
-        <button type="button" class="action-btns" onclick="handleLowerCase()">LowerCase</button>
-        <button type="button" class="action-btns" onclick="handleWordCount()">Word Count</button>
-        <button type="button" class="action-btns" onclick="handleCharacterCount()">Character Count</button>
-        <button type="button" class="clear-btn" onclick="clearInput()">Clear</button>
-    </div>
-</body>
-</html>
+function handleUpperCase() {
+    const inputText = inputElement.value;
+    outputElement.value = inputText.toUpperCase();
+}
+
+function handleLowerCase() {
+    const inputText = inputElement.value;
+    outputElement.value = inputText.toLowerCase();
+}
+
+function handleWordCount() {
+    const totalWords = inputElement.value.split(" ").length;
+    outputElement.value = `Total Words are : ${totalWords}`;
+}
+
+function handleCharacterCount() {
+    const inputText = inputElement.value;
+    outputElement.value = `Character count : ${inputText.length}`;
+}
+
+function clearInput() {
+    inputElement.value = '';
+    outputElement.value = '';
+}
